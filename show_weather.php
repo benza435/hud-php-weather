b3296bc06dfee3ad1a1774e046f557294aad8c4a
ben <benegan435@gmail.com>
2021-06-26 14:05:03 +0100

    app now shows data from local db

---
+++ show_weather.php
@@ -0,0 +1,46 @@
+<html>
+  <body>
+
+<h1>data from show_weather.data_cache:</h1>
+<?php
+include 'connection.php';
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM data_cache";
+$weather = $conn->query($sql);
+
+//$update = "SELECT updated FROM data_cache LIMIT 1";
+//$last_update = $conn->query($update);
+//echo $last_update;
+
+if ($weather->num_rows > 0) {
+
+  echo "<table border=1><tr>";
+  // output data of each row
+  while($row = $weather->fetch_assoc()) {
+    echo "<td style='padding:10px'>"
+    ."<h2>".$row["format_day"]."</h2><br>"
+    ."<h3>".$row["overview"]."</h3><br>"
+    ."<span>".$row["tempmax"]."</span><br>"
+    ."<span>".$row["tempmin"]."</span><br>"
+    ."<span>".$row["rainchance"]."</span><br>"
+    ."<span>".$row["wind"]."</span><br>"
+    ."</td>";
+  }
+  echo "</tr></table>";
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+
+
+</body>
+</html>
+
