9cc806c3473de34fb6101e732c1816d9a46e38ab
ben <benegan435@gmail.com>
2021-06-27 18:40:54 +0100

    add screenshots

--- db/create-tables.sql
+++ db/create-tables.sql
@@ -2,12 +2,13 @@ USE weather_test;
 DROP TABLE IF EXISTS data_cache;
 CREATE TABLE data_cache (
     updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    format_day VARCHAR(20),
+    forecast_day VARCHAR(20),
     overview INT,
     tempmax INT,
     tempmin INT,
     rainchance INT,
-    wind INT);
+    wind_min INT,
+    wind_max INT);
 
 DROP TABLE IF EXISTS significant_codes;
 CREATE TABLE significant_codes (
